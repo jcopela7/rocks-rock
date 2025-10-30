7ec0f7751b6d7cf068cd59558b9480a06926824d
Jonathan Cope <jonathancope@Jonathans-MacBook-Pro.local>
2025-10-30 12:27:13 -0400

    feat(web): skeleton for web admin portal

---
+++ web/vite.config.ts
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: { dedupe: ['react', 'react-dom'] },
+  optimizeDeps: { include: ['react', 'react-dom', '@geist-ui/core'] }
+})
